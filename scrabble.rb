eabe416cbb92d98bd49bf0be23ae63ff9e625e74
David Cruz <dcruz112@gmail.com>
2013-06-03 13:54:09 -0400

    Added height and width variable and parsed board. Also added code to create list of possible words

--- scrabble.rb
+++ scrabble.rb
@@ -1,52 +1,92 @@
-class Scrabble
+# lines = array of the input file
+lines=[]
 
-	# lines = array of the input file
-	lines=[]
-	File.open("INPUT.json") do |file|
-		lines = file.map { |line| line }
-	end
-
-	# board = integer of where board appears in array
-	board = lines.index{|e| e=~ /board/}
-
-	# dictionary = integer of where dictionary appears in array
-	dictionary = lines.index{|e| e=~ /dictionary/}
-
-	# tiles = integer of where tiles appears in array
-	tiles = lines.index{|e| e=~ /tiles/}
-
-	puts "this is the dictionary:"
-	# array of the words: "word",
-	dictionary_array = []
-	start_words = dictionary
-	while start_words < (tiles - 2) do
-		dictionary_array << lines[start_words + 1].gsub(/\W/, '')
-		start_words += 1
-	end
-	puts dictionary_array
-	puts ""
-
-	puts "these are the tiles:"
-	# array of the tiles: "a1",
-	tile_array = []
-	tile_letters = []
-	start_tiles = tiles
-	while start_tiles < (lines.length - 3) do
-		# tile = lines[start_tiles + 1].gsub('a', 'A')
-		tile_array << lines[start_tiles + 1].gsub(/\W/, '')
-		start_tiles += 1
-	end
-
-	# create tile_letters, same as tile_arrays
-	a = 0
-	while a < tile_array.length do
-		tile_letters << tile_array[a]
-		a += 1
-	end
-
-	# tile_letters.gsub('a', 'A')
-
-	puts "TILE array"
-	puts tile_array
-
+File.open("INPUT.json") do |file|
+	lines = file.map { |line| line }
 end
+
+# board = integer of where board appears in array
+board = lines.index{|e| e=~ /board/}
+
+# dictionary = integer of where dictionary appears in array
+dictionary = lines.index{|e| e=~ /dictionary/}
+
+# tiles = integer of where tiles appears in array
+tiles = lines.index{|e| e=~ /tiles/}
+
+board_array = []
+start_board = board
+
+while start_board < (dictionary - 2) do
+	board_line = lines[start_board + 1].gsub(/\D/, '').split(//)
+	board_array << board_line
+	start_board += 1
+end
+
+height = board_array.length
+width = board_array[0].length
+
+# array of the words: "word",
+
+dictionary_array = []
+start_words = dictionary
+
+while start_words < (tiles - 2) do
+	word = lines[start_words + 1].gsub(/\W/, '')
+	if word.length <= width || word.length <= height
+		dictionary_array << word
+	end
+	start_words += 1
+end
+
+# array of the tiles: "a1",
+
+tile_array = []
+start_tiles = tiles
+
+while start_tiles < (lines.length - 3) do
+	tile_array << lines[start_tiles + 1].gsub(/\W/, '')
+	start_tiles += 1
+end
+
+# create tile_letters, same as tile_arrays
+
+tile_letters = []
+
+tile_array.each do |piece|
+	tile_letters << piece[0]
+end
+
+moves = []
+
+#For each word, this loop checks if it is possible, given the tiles
+
+dictionary_array.each do |current_word|
+	makeable = true
+	word_letters = current_word.split(//) 
+		#Turns the word into an array of its letters
+	tiles_dup = tile_letters.dup 
+		#Creates a duplicate array of available tiles
+
+	#For each letter in the word, the loop checks if the current letter is an 
+	#available tile in the duplicate list if it is possible, the letter is 
+	#deleted from word_letters and from the list of available tiles
+
+	word_letters.each do |current_letter|
+		if tiles_dup.include? current_letter
+			tiles_dup.delete_at(tiles_dup.index(current_letter))
+		else
+			makeable = false
+			break
+		end
+	end
+
+	#If there are any letters that did not match to a tile, then valid will be 
+	#false. Otherwise, it is added to the array of possible words
+
+	if makeable
+		moves << current_word
+	end
+end
+
+puts moves
