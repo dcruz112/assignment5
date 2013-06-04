aee7be88278c2b5c1be148d92d017791c4b79f61
David Cruz <dcruz112@gmail.com>
2013-06-04 16:57:01 -0400

    So many classes

--- scrabble.rb
+++ scrabble.rb
@@ -24,18 +24,18 @@ class Parser
 end
 
 class Board
-	# @@board = []
+
 	def initialize
 		parser = Parser.new
 		@board = parser.make_arrays(parser.board, parser.dictionary, "board")
 	end
 
 	def height
-		return @board[0].length
+		return @board.length
 	end
 
 	def width
-		return @board.length
+		return @board[0].length
 	end
 
 	def mults
@@ -44,93 +44,105 @@ class Board
 
 end
 
+class TileSet
 
-# lines = array of the input file
-lines=[]
-
-File.open("INPUT.json") do |file|
-	lines = file.map { |line| line }
-end
-
-# board = integer of where board appears in array
-board = lines.index{|e| e=~ /board/}
-
-# dictionary = integer of where dictionary appears in array
-dictionary = lines.index{|e| e=~ /dictionary/}
-
-# tiles = integer of where tiles appears in array
-tiles = lines.index{|e| e=~ /tiles/}
-
-info = Hash.new([])
-
-def make_arrays(target, ref, lines, info, symbol)
-	xray = []
-	while target < (ref - 2) do
-		x = lines[target + 1].gsub(/\W/, '')
-		x = x.split(//) if symbol == "board"
-		xray << x
-		info[symbol.to_sym] = xray
-		target += 1
+	def initialize
+		parser = Parser.new
+		dict = Dictionary.new
+		tHash = Hash.new([])
+		@pieces = parser.make_arrays(parser.tiles, parser.lines.length - 1, "tiles")
+		@tile_letters = []
+		@tile_vals = []
+		@moves = dict.possible_moves
+		@word_values = []
+		@letters = []
+		@num = 0
 	end
+
+	def letters
+		@pieces.each do |piece|
+			@tile_letters << piece[0]
+			tHash[piece[0].to_sym] = piece.gsub(/\D/, '').to_i
+		end
+		return @tile_letters
+	end
+
+	#Turn given word into array of its letters' values
+	
+	def values
+		@moves.each do |current_word|
+			@letters = current_word.split(//)
+			@letters.each do |current_letter|
+				@num = tHash[current_letter.to_sym]
+				@word_values << num
+			end
+			@tile_vals << @word_values
+		end
+		return @tile_vals
+	end
+
 end
 
+class Dictionary
+
+	attr_reader :makeable
+
+	def initialize
+		parser = Parser.new
+		tiles = TileSet.new
+		@dictionary = parser.make_arrays(parser.dictionary, parser.tiles, "dictionary")
+		@moves = []
+		@word_letters = []
+		@makeable = true
+		@tiles_dup = tiles.letters
+	end
+
+	def possible_moves
+
+		#For each word, this loop checks if it is possible, given the tiles
+
+		@dictionary.each do |current_word|
+			
+			@word_letters = current_word.split(//) 
+				#Turns the word into an array of its letters
+
+			#For each letter in the word, the loop checks if the current letter is an 
+			#available tile in the duplicate list if it is possible, the letter is 
+			#deleted from word_letters and from the list of available tiles
+
+			@word_letters.each do |current_letter|
+				if @tiles_dup.include? current_letter
+					@tiles_dup.delete_at(@tiles_dup.index(current_letter))
+				else
+					@makeable = false
+					break
+				end
+			end
+
+			#If there are any letters that did not match to a tile, then valid will be 
+			#false. Otherwise, it is added to the array of possible words
+
+			if @makeable
+				@moves << current_word
+			end
+		end
+
+		return @moves
+
+	end
+
+end
 
 board = Board.new
 field = board.mults
 
-tile_letters = []
-tHash = Hash.new([])
+tiles = TileSet.new
+pieces = tiles.pieces
+tile_letters = tiles.letters
+tile_vals = tiles.values
 
-info[:tiles].each do |piece|
-	tile_letters << piece[0]
-	tHash[piece[0].to_sym] = piece.gsub(/\D/, '').to_i
-end
-
-moves = []
-
-#For each word, this loop checks if it is possible, given the tiles
-
-info[:dictionary].each do |current_word|
-	makeable = true
-	word_letters = current_word.split(//) 
-		#Turns the word into an array of its letters
-	tiles_dup = tile_letters.dup 
-		#Creates a duplicate array of available tiles
-
-	#For each letter in the word, the loop checks if the current letter is an 
-	#available tile in the duplicate list if it is possible, the letter is 
-	#deleted from word_letters and from the list of available tiles
-
-	word_letters.each do |current_letter|
-		if tiles_dup.include? current_letter
-			tiles_dup.delete_at(tiles_dup.index(current_letter))
-		else
-			makeable = false
-			break
-		end
-	end
-
-	#If there are any letters that did not match to a tile, then valid will be 
-	#false. Otherwise, it is added to the array of possible words
-
-	if makeable
-		moves << current_word
-	end
-end
-
-# turn given word into array of its letters' values
-# array of arrays
-tile_vals = []
-
-moves.each do |current_word|
-	word_values = []
-	the_letters = current_word.split(//)
-	the_letters.each do |current_letter|
-		num = tHash[current_letter.to_sym] #.to_i
-		word_values << num
-	end
-	tile_vals << word_values
-end
+dict = Dictionary.new
+moves = dict.possible_moves
 
 max = { :val => 0, :word => "", :row => 0, :col => 0, :horiz => true }
 
