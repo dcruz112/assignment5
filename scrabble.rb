8de54841760901d4e9ca8f865edb7a670137a0e3
djerek <derekgritz@yahoo.com>
2013-06-05 13:26:37 -0400

    Write to output file

--- scrabble.rb
+++ scrabble.rb
@@ -2,20 +2,21 @@ class Parser
 	attr_accessor :board, :dictionary, :tiles
 
 	def initialize
-		# make array of lines of the input file
+		# Make array of lines of the input file
 		@lines = []
 		File.open("INPUT.json") do |file|
 			@lines = file.map { |line| line }
 		end
 
-		# find where in the array these words appear
+		# Find where in the array these words appear
 		@board = @lines.index{|e| e=~ /board/}
 		@dictionary = @lines.index{|e| e=~ /dictionary/}
 		@tiles = @lines.index{|e| e=~ /tiles/}
 		@info = Hash.new([])
 	end
 
-	# return an array containing just the desired content
+	# Return an array containing just the desired content
+
 	def make_arrays(target, ref, symbol)
 		xray = []
 		while target < (ref - 2) do
@@ -57,30 +58,34 @@ class TileSet
 
 	def initialize
 		parser = Parser.new
-		@tHash = Hash.new([])
 		@pieces = parser.make_arrays(parser.tiles, parser.length - 1, "tiles")
-		@tile_letters = []
-		@tile_vals = []
-		@word_values = []
-		@letters = []
-		@num = 0
 	end
 
+	# Creates an array with the letters of each tiles
+
 	def letters
+		@tile_letters = []
 		@pieces.each do |piece|
 			@tile_letters << piece[0]
 		end
 		return @tile_letters
 	end
+
+	# Creates an array with the values of each tiles
 	
 	def values
+		@tile_vals = []
 		@pieces.each do |piece|
 			@tile_vals << piece.gsub(/\D/, '').to_i
 		end
 		return @tile_vals
 	end
 
+	# Creates a hash with the letters as a key and the numbers as values
+
 	def table
+		@tHash = Hash.new([])
+		@tile_letters = letters
 		@tile_vals = values
 		@pieces.each_with_index do |piece, index|
 			@tHash[@tile_letters[index].to_sym] = @tile_vals[index]
@@ -98,7 +103,6 @@ class Dictionary
 		tiles = TileSet.new
 		@dictionary = parser.make_arrays(parser.dictionary, parser.tiles, "dictionary")
 		@moves = []
-		@makeable = true
 		@tile_list = tiles.letters
 		@table = tiles.table
 	end
@@ -106,15 +110,18 @@ class Dictionary
 	def possible_moves
 
 		# For each word, this loop checks if it is possible, given the tiles
+
 		@dictionary.each do |current_word|
 			@makeable = true
 			@tiles_dup = @tile_list.dup
 			word_letters = current_word.split(//) 
-				#Turns the word into an array of its letters
+				# Turns the word into an array of its letters
 
 			# For each letter in the word, the loop checks if the current letter 
 			# is an available tile in the duplicate list. If it is possible, the 
-			# letter is deleted from word_letters and the list of available tiles
+			# letter is deleted from word_letters and the list of available 
+			# tiles
+
 			word_letters.each do |current_letter|
 				if @tiles_dup.include? current_letter
 					@tiles_dup.delete_at(@tiles_dup.index(current_letter))
@@ -125,7 +132,9 @@ class Dictionary
 			end
 
 			# If there are any letters that did not match to a tile, then valid 
-			# will be false. Otherwise, it's added to the array of possible words
+			# will be false. Otherwise, it's added to the array of possible 
+			# words
+
 			if @makeable
 				@moves << current_word
 			end
@@ -133,10 +142,12 @@ class Dictionary
 		return @moves
 	end
 
-	# turn makeable words into array of their letters, puts each array into
+	# Turns makeable words into array of their letters, puts each array into
 	# vals, an array containing arrays of each words' letters
+
 	def move_vals
 		vals = []
+		@moves = possible_moves
 		@moves.each do |word|
 			word_letters = word.split(//)
 			word_vals = []
@@ -155,43 +166,80 @@ class Scorer
 
 	attr_accessor :max
 
+	# Initializes the hash 'max' and gives the scorer access to the board
+
 	def initialize
 		@max = { :val => 0, :word => "", :row => 0, :col => 0, :horiz => true }
-		@point_value = 0
 		board = Board.new
 		@field = board.mults
 	end
 
+	# Calculates the score by multiplying the multipliers from each possible
+	# move by the value of each letter
+
 	def check_score(bound, incr_coord, fixed_coord, piece, word, horiz)
 		@total = 0
 		@i = 0
+		@pt = 0
 		@len = piece.length
+		
+		# If the word will fit in the space left on the board (between the edge
+		# and the current position being tested)
+
 		if bound - incr_coord >= @len
+			
+			# Rather than increment the variable incr_coord, we make a copy so 
+			# when we go to store the starting position in max, incr_coord
+			# has the correct value
+
 			@c = incr_coord
+			
+			# i increments through the length of word
+
 			while @i < @len do
+
+				# Depending on whether the word reads horizontally or not, the
+				# fixed_coord is in either the row or column position
+
 				@pt = horiz ? @field[fixed_coord][@c] : @field[@c][fixed_coord]
 				@total += @pt.to_i * piece[@i]
+					# This multiplies the multiplier on the board by the value 
+					# of the letter. i and c are then incremented by 1
 				@i += 1
 				@c += 1
 			end
 			check_max(@total, word, horiz, fixed_coord, incr_coord)
+				# This checks if the new total is greater than the max score.
 		end
 
 		return @max
 	end
 
-	def check_max(comp_total, comp_word, bool, comp_fcoord, comp_icoord)
+	# Compares the current score to the maximum score. If it is greater, all
+	# information about the current configuration is stored in the hash 'max'
+
+	def check_max(comp_total, comp_word, horiz, comp_fcoord, comp_icoord)
 		if @max[:val] < comp_total
+
 			@max[:val] = comp_total
 			@max[:word] = comp_word
-			if bool
+
+			# If the word is horizontal (reads left to right), then it increments
+			# along the column.
+
+			if horiz
 				@max[:row] = comp_fcoord
 				@max[:col] = comp_icoord
+
+			# Otherwise, it reads from top to bottom, and increments by row
+
 			else
 				@max[:row] = comp_icoord
 				@max[:col] = comp_fcoord
 			end
-			@max[:horiz] = bool
+			
+			@max[:horiz] = horiz
+
 		end
 	end
 
@@ -211,6 +259,7 @@ max = scorer.max
 # word at a time. Finds words that are possible by checking with dictionary.
 # Then it checks the scores going through the rows and columns and finds
 # the biggest word and its position.
+
 vals.each do |piece|
 	word = moves[vals.index(piece)]
 	field.each_with_index do |line, index_row|
@@ -222,6 +271,7 @@ vals.each do |piece|
 end
 
 # Turns biggest word into an array so it can be placed in the output board.
+
 word_letters = max[:word].split(//)
 
 word_letters.each do |letter|
